f752685cea8ce619de5c0b59fdff212d1c6f43d8
flozoswoops <flores.a.mark@gmail.com>
2017-11-01 13:06:15 -0400

    styled navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom'
 import HomePage from './components/HomePage'
 import NavBar from './components/NavBar'
+
 class App extends Component {
   render() {
     return (
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
+import styled from 'styled-components'
 
+const Nav = styled.div`
+display: flex;
+justify-content: space-between;
+font-size: 20px;
+color: blue;
+text-align: center;
+background-color: rgba(25, 100, 130, .25);
+
+a {
+  text-decoration: none;
+  color: blue;
+}
+`
 class NavBar extends Component {
     render() {
         return (
-            <div>
+            <Nav>
               
             <Link to ="/"> Home Page </Link>
             <Link to ="/"> Sign Up </Link>
-            <Link to ="/"> Login In  </Link>
+            <Link to ="/"> Log In  </Link>
             
 
 
-            </div>
+            </Nav>
         );
     }
 }
