a884298733c4b2427e99fffa28b622a16a16c7c7
flozoswoops <flores.a.mark@gmail.com>
2017-11-03 12:14:34 -0400

    made more changes seeds

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :city
   validates :content, presence: true
-  validates :tile, presence: true, length: { minimum: 1 }, length: { maximum: 200 }
+  validates :title, presence: true, length: { minimum: 1,  maximum: 200 }
 end
